682555b3596fe581c71e9be942f65e0319734781
ritiksharmarj <bloggeritik@gmail.com>
2022-10-29 01:50:08 +0530

    Toast Message.

--- app/src/main/java/com/ritiksharmarj/lemonade/MainActivity.kt
+++ app/src/main/java/com/ritiksharmarj/lemonade/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.ritiksharmarj.lemonade
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        image_lemon_state.setOnClickListener {
+            Toast.makeText(this, "Pick a lemon", Toast.LENGTH_SHORT).show()
+        }
     }
 }
